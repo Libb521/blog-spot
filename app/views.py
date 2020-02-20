597b00d136d8df7ee1e9c4d12dde078c47fc29fa
Libb521 <eotieno39@yahoo.com>
2020-02-20 11:55:26 +0300

    created some dynamic routes

--- app/views.py
+++ app/views.py
@@ -1,9 +1,20 @@
 from flask import render_template
 from app import app
+# from ..models import User, Blogpost
 
 #views
 @app.route('/')
 def index():
 
-    message = 'Libby Blog spot'
-    return render_template('index.html', message = message)
+    # posts = Blogpost.query.all()
+    title = 'Blog spot'
+    message = 'Welcome to Libby blogs'
+    return render_template('index.html', message = message, title = title)
+
+@app.route('/blog')
+# @login_required
+def blog():
+
+    # posts = Blogpost.query.all()
+    title = 'Blog spot'
+    return render_template('blog.html', title = title)
